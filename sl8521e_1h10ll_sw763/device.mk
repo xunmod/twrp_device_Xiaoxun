222acac143ad1c8011eb026a94be39e59b19d8d2
strobecat <203321455+strobecat@users.noreply.github.com>
2026-06-08 00:54:58 +0800

    try ccache

--- sl8521e_1h10ll_sw763/device.mk
+++ sl8521e_1h10ll_sw763/device.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/Xiaoxun/sl8521e_1h10ll-common/common.mk)
 
 PRODUCT_COPY_FILES += \
     device/Xiaoxun/sl8521e_1h10ll_sw763/prebuilt/kernel:kernel \
