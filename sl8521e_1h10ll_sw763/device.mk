a5684aa15fc75cf52ef064bc3ea7c428f35e43b5
strobecat <203321455+strobecat@users.noreply.github.com>
2026-06-08 00:38:58 +0800

    fix build, try remove rootdir

--- sl8521e_1h10ll_sw763/device.mk
+++ sl8521e_1h10ll_sw763/device.mk
@@ -1,5 +1,4 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/Xiaoxun/sl8521e_1h10ll-common/common.mk)
 
 PRODUCT_COPY_FILES += \
     device/Xiaoxun/sl8521e_1h10ll_sw763/prebuilt/kernel:kernel \
